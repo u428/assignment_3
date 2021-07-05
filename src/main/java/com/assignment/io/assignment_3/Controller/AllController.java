fcf71677243214540459a396b6b3db4702a53fc9
root <qurbanovulugbek428@gmail.com>
2021-07-05 16:53:43 +0500

    authority

--- src/main/java/com/assignment/io/assignment_3/Controller/AllController.java
+++ src/main/java/com/assignment/io/assignment_3/Controller/AllController.java
@@ -4,6 +4,7 @@ import com.assignment.io.assignment_3.Model.Entity.Product;
 import com.assignment.io.assignment_3.Service.OtherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.MalformedURLException;
@@ -16,21 +17,25 @@ public class AllController {
     @Autowired
     private OtherService otherService;
 
+    @PreAuthorize("hasAuthority('read')")
     @GetMapping(path = "/sortProductByCategory/{id}")
     public ResponseEntity sortProductByCategory(@PathVariable Long id){
         return otherService.sortProductByCategory(id);
     }
 
+    @PreAuthorize("hasAuthority('read')")
     @GetMapping(path = "/getProducts")
     public ResponseEntity<List<Product>> getAllProduct(){
         return otherService.getAllProducts();
     }
 
+    @PreAuthorize("hasAuthority('read')")
     @GetMapping(path = "/getproduct/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id){
         return otherService.getProductById(id);
     }
 
+    @PreAuthorize("hasAuthority('read')")
     @GetMapping(path = "/image/{id}")
     public ResponseEntity image(@PathVariable Long id) throws MalformedURLException {
         return otherService.image(id);
